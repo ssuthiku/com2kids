820dcdc47cfa7106cbad1c4aa02505a411c065b7
Suravee Suthikulpanit <suravee@gmail.com>
2018-01-12 12:44:25 +0700

    Fix installation script

--- scripts/usage-tracker/install.sh
+++ scripts/usage-tracker/install.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
+rm -f com2kids
 wget https://raw.githubusercontent.com/ssuthiku/com2kids/master/scripts/usage-tracker/com2kids
+sudo chmod +x com2kids
 sudo cp com2kids /etc/init.d/
 update-rc.d -f com2kids remove
 update-rc.d -f com2kids defaults
