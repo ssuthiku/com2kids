bd9e87b117159ca32454e5c6a0826668e8a25425
Suravee Suthikulpanit <suravee@gmail.com>
2018-01-12 10:44:58 +0700

    Update install script to download the service script via wget

--- scripts/usage-tracker/install.sh
+++ scripts/usage-tracker/install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-update-rc.d -f com2kids remove
+wget https://raw.githubusercontent.com/ssuthiku/com2kids/master/scripts/usage-tracker/com2kids
 sudo cp com2kids /etc/init.d/
+update-rc.d -f com2kids remove
 update-rc.d -f com2kids defaults
